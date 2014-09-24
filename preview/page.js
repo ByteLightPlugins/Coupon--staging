4f4646a51cece8787bce4255fff57e39f78d4072
ByteLight Plugin Repositories <plugins@bytelight.com>
2014-09-24 14:26:20 +0000

    Released on: 2014-09-24 14:26:15 +0000

--- preview/page.js
+++ preview/page.js
@@ -1,8 +1,22 @@
-$('#buttonText, hr').on('click', function() {
-  $('#overlay').css('display', 'block');
+var appSelector = '#template_' + APP.appId;
+
+$(appSelector + ' #buttonText').on('click', function() {
+  $(appSelector + ' #overlay').css('display', 'block');
   JSNativeCom.sendEventToNative('OFFER_SAVED');
 })
 
 BL.addTemplateResetFunction(function() {
-  $('#overlay').css('display', 'none');
+  $(appSelector + ' #overlay').css('display', 'none');
 })
+
+if (BL.isTablet) {
+  $(appSelector + ' #templateDiv').addClass('tablet-version');
+    
+  $(appSelector + ' p, ' + appSelector + ' span').forEach(function(p) {
+    fontSize = $(p).css('font-size');
+    fontSize = fontSize.substring(0, fontSize.length - 2);
+    fontSize = parseInt(fontSize);
+    $(p).css('font-size', fontSize * 1.5);
+  })
+    
+}
